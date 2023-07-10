a3366ea3d53445ada28349a6f13700d086a8e6ba
Mayank-Bihani <82774484+Mayank-Bihani@users.noreply.github.com>
2023-07-10 22:16:15 +0530

    index.js

--- index.js
+++ index.js
@@ -26,3 +26,66 @@ function animateValue(obj, start, end, duration,showplus){
   animateValue(port, 0, 10000, 2000,false);
   animateValue(cargo, 0, 30000, 2000,true);
   animateValue(container, 0, 700000, 2000,false);
+
+
+
+// For color change of navbar
+const navBar = document.querySelector('.nav');
+
+window.addEventListener('scroll', () => {
+  const y = window.scrollY;
+  
+  if (y==0){
+  navBar.style.backgroundColor = `#fbdc00`;
+  navBar.style.boxShadow="0 0 0 white";
+
+  }
+  else{
+  navBar.style.backgroundColor = `white`;
+  navBar.style.boxShadow="0 1px 6px rgba(169,188,218,.65)";
+
+  }
+})
+
+
+
+
+const op1 = document.querySelector(".productContainer");
+const  sb1 = op1.querySelector(".productButton");
+
+
+const op2 = document.querySelector(".partnerContainer");
+const  sb2 = op2.querySelector(".partnerButton");
+
+
+const op3 = document.querySelector(".toolContainer");
+const  sb3 = op3.querySelector(".toolButton");
+
+
+const op4 = document.querySelector(".companyContainer");
+const  sb4 = op4.querySelector(".companyButton");
+
+
+
+const op5 = document.querySelector(".knowledgeContainer");
+const  sb5 = op5.querySelector(".knowledgeButton");
+
+sb1.addEventListener("click", () =>
+  op1.classList.toggle("active")
+);
+
+sb2.addEventListener("click", () =>
+  op2.classList.toggle("active")
+);
+
+sb3.addEventListener("click", () =>
+  op3.classList.toggle("active")
+);
+
+sb4.addEventListener("click", () =>
+  op4.classList.toggle("active")
+);
+
+sb5.addEventListener("click", () =>
+  op5.classList.toggle("active")
+);
